7944ef561a52f65e06d04842a3a414c1d8160ba7
ZacK1245 <53895630+ZacK1245@users.noreply.github.com>
2022-03-28 21:55:38 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import string
+import random
+
+lowercap= string.ascii_letters+string.digits
+
+print(random.choice(lowercap)+random.choice(lowercap)+random.choice(lowercap)+random.choice(lowercap)+random.choice(lowercap)+random.choice(lowercap)+random.choice(lowercap)+random.choice(lowercap)+random.choice(lowercap)+random.choice(lowercap)+random.choice(lowercap)+random.choice(lowercap)+random.choice(lowercap)+random.choice(lowercap))
+
